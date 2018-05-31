80bb351175a8bfe6ca0ad9fb4dc334e1be6df548
Elliot Smith <esmith2642@gmail.com>
2018-05-31 16:07:00 -0500

    csv writing

--- WriteReadCSV/write_read_csv.py
+++ WriteReadCSV/write_read_csv.py
@@ -1,1 +1,12 @@
-print(1)
+import csv
+
+cols_array = [[1, 2, 3], [4, 5, 6]]
+
+f = open('numbers.csv', 'w')
+
+with f:
+
+    writer = csv.writer(f)
+
+    for row in cols_array:
+        writer.writerow(row)
