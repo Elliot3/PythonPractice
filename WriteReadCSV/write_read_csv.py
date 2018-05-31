ab3bf4d80cc9a3b0a45919ba32998a1e02311d25
Elliot Smith <esmith2642@gmail.com>
2018-05-31 17:01:10 -0500

    read csv

--- WriteReadCSV/write_read_csv.py
+++ WriteReadCSV/write_read_csv.py
@@ -1,6 +1,6 @@
 import csv
 
-cols_array = [[1, 2, 3], [4, 5, 6]]
+data_array = [['col1', 'col2', 'col3'], [1, 2, 3], [4, 5, 6]]
 
 f = open('numbers.csv', 'w')
 
@@ -8,5 +8,42 @@ with f:
 
     writer = csv.writer(f)
 
-    for row in cols_array:
+    for row in data_array:
         writer.writerow(row)
+
+# Read as a List, skip headers
+
+with open('numbers.csv') as f:
+    reader = csv.reader(f)
+    next(reader)
+    data = []
+
+    for row in reader:
+        data.append(row)
+
+    # data.pop(0)
+
+# print(data)
+
+# Read as a List, include headers
+
+with open('numbers.csv') as f:
+    reader = csv.reader(f)
+    data = []
+
+    for row in reader:
+        data.append(row)
+
+# print(data)
+
+# Read as a Dict
+
+with open('numbers.csv') as f:
+    reader = csv.DictReader(f)
+    data = []
+
+    for row in reader:
+        data.append(row)
+
+# out = data[1]['col2']
+# print(out)
