224dbfec4ba4df5c9cb0a4c6628fe2f4a1dfa099
Elliot Smith <esmith2642@gmail.com>
2018-06-01 11:52:27 -0500

    plotting practice

--- CoinFlip/coin_flip.py
+++ CoinFlip/coin_flip.py
@@ -6,7 +6,7 @@ def coin_flip(number):
     flip_wins, heads, tails = [], 0, 0
 
     for i in range(number):
-        
+
         flip = random.randint(0, 1)
 
         if flip == 0:
--- SimpleRegression/simple_regression.py
+++ SimpleRegression/simple_regression.py
@@ -1,16 +1,37 @@
 import statsmodels.api as sm
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 from sklearn import datasets
 
 data = datasets.load_boston()
 df = pd.DataFrame(data.data, columns=data.feature_names)
 target = pd.DataFrame(data.target, columns=["MEDV"])
 
+# Obtain the predictor and response
+
 X = df["RM"]
 y = target["MEDV"]
 
+# Log transform the predictor
+
+X = np.log10(X)
+
+# Build the model
+
 model = sm.OLS(y, X).fit()
+
+# Extract the fitted values
+
 predictions = model.predict(X)
+
+# Save the model summary and print it
 
 out = model.summary()
 print(out)
+
+# Plot the best fit line
+
+plt.plot(X, predictions)
+plt.legend()
+plt.show()
